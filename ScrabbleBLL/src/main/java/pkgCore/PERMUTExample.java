b7bb8aed321b2458b68af43096a3aa022e568f1e
CISC181 <bgibbons@udel.edu>
2022-09-20 13:44:45 -0400

    Getting ready for Java17...

--- ScrabbleBLL/src/main/java/pkgCore/PERMUTExample.java
+++ ScrabbleBLL/src/main/java/pkgCore/PERMUTExample.java
@@ -14,63 +14,36 @@ public class PERMUTExample {
 	{
 		ArrayList<String> combin = COMBINExample.Combin("ABCDE");
 		
-		ArrayList<Word> permut = GeneratePossibleWords(combin);
-		int WordNumber = 0;
-		System.out.println("********  Permut words ********");
+		ArrayList<String> words = new ArrayList<String>();
 		
-		for (Word w: permut)
+		for (String strPossibleWord : combin)
 		{
-			System.out.println(w.getWord());
+			ArrayList<Character> arrLetters = new ArrayList<Character>();
+
+			for (int i = 0; i < strPossibleWord.length(); i++) {
+				char c = strPossibleWord.charAt(i);
+				arrLetters.add(c);
+			}
+			
+			Collection<List<Character>> ch = Collections2.orderedPermutations(arrLetters);
+			for (final List<Character> p : ch) {
+				{
+					String strBuild = "";
+					for (Character chrs : p) {
+						strBuild = strBuild + chrs;
+					}
+					words.add(strBuild);
+				}
+			}
 		}
+		
+		for (String w: words)
+		{
+			System.out.println(w);
+		}
+		
+		
 		
 	}
 	
-	/**
-	 * GeneratePossibleWords - Pass in an ArrayList of Strings and 
-	 * for every String, call it's sister method.
-	 * 
-	 *  Return all the words found to a HashSet, because you want to remove duplicates
-	 *  
-	 *  After the HashSet is complete with non-duplicate entries, cast it back to an ArrayList
-	 *  and sort the findings.  Return the sorted ArrayList to the user.
-	 * @param arrLetters
-	 * @return
-	 */
-	private static ArrayList<Word> GeneratePossibleWords(ArrayList<String> arrLetters) {
-		HashSet<Word> words = new HashSet<Word>();
-
-		for (String strPossibleWord : arrLetters) {
-			words.addAll(GeneratePossibleWords(strPossibleWord));
-		}
-		ArrayList<Word> myWords = new ArrayList<Word>(words);
-		Collections.sort(myWords, Word.CompWord);
-		return myWords;
-	}
-
-	/**
-	 * GeneratePossibleWords - A string is passed, and this method will figure out 
-	 * every permutation of the string.  Add the permuted strings to a HashSet and pass it back
-	 * @param strLetters
-	 * @return
-	 */
-	private static HashSet<Word> GeneratePossibleWords(String strLetters) {
-		HashSet<Word> hsPossibleWords = new HashSet<Word>();
-		ArrayList<Character> arrLetters = new ArrayList<Character>();
-
-		for (int i = 0; i < strLetters.length(); i++) {
-			char c = strLetters.charAt(i);
-			arrLetters.add(c);
-		}
-		Collection<List<Character>> ch = Collections2.orderedPermutations(arrLetters);
-		for (final List<Character> p : ch) {
-			{
-				String strBuild = "";
-				for (Character chrs : p) {
-					strBuild = strBuild + chrs;
-				}
-				hsPossibleWords.add(new Word(strBuild));
-			}
-		}
-		return hsPossibleWords;
-	}
 }
